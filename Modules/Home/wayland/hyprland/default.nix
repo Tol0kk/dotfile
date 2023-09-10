d6810236a13a56f18ccf6c821713b93e644d7ff3
Tol0kk <titoaunledilavrec@gmail.com>
2023-09-10 23:22:49 +0200

    add waybar as depedencie for hyprland

--- Modules/Home/wayland/hyprland/default.nix
+++ Modules/Home/wayland/hyprland/default.nix
@@ -20,6 +20,7 @@ in
       xwayland.enable = true;
       enableNvidiaPatches = true;
     };
+    programs.waybar.enable = true;
     xdg.configFile."hypr/hyprland.conf".source = ./hyprland.conf;
     home.packages = with pkgs; [
       hyprpicker
