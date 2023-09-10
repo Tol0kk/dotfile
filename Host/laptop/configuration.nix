d6152744590495a7a448b272ce4a1731f8042b73
Tol0kk <titoaunledilavrec@gmail.com>
2023-09-10 23:18:46 +0200

    update laptop conf to match modules refactor

--- Host/laptop/configuration.nix
+++ Host/laptop/configuration.nix
@@ -1,19 +1,5 @@
 { self, lib, config, pkgs, inputs, ... } @ inputss:
 {
-  environment.sessionVariables = {
-    XCURSOR_SIZE = "128";
-    XCURSOR_PATH = [
-      "${config.system.path}/share/icons"
-      "$HOME/.icons"
-      "$HOME/.nix-profile/share/icons/"
-    ];
-    GTK_DATA_PREFIX = [
-      "${config.system.path}"
-    ];
-    NIXOS_DOTFILE_DIR = "${self}";
-  };
-
-
   sound.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -23,10 +9,6 @@
     pulse.enable = true;
   };
 
-  programs.steam.enable = true;
-  hardware.steam-hardware.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-
   nix.settings = {
     # for nix develop to keep derivation
     keep-outputs = true;
@@ -35,52 +17,11 @@
 
   console.font = "${pkgs.terminus_font}/share/consolefonts/ter-132b.psf.gz";
 
-  # Bootloader
-  boot.loader = {
-    timeout = 1;
-    efi.canTouchEfiVariables = true;
-    grub = {
-      enable = true;
-      device = "nodev";
-      efiSupport = true;
-      gfxmodeEfi = "3840x2400";
-      fontSize = 36;
-      font = "${pkgs.hack-font}/share/fonts/hack/Hack-Regular.ttf";
-      extraEntries = '' 
-       menuentry "Windows" {
-        insmod part_gpt
-        insmod fat
-        search --no-floppy --fs-uuid --set=root B4C8-4FD5
-        chainloader /efi/Microsoft/Boot/bootmgfw.efi
-       }
-       menuentry "Reboot" {
-        reboot
-       }
-       menuentry "Poweroff" {
-        halt
-       }
-      '';
-    };
-  };
   services.fwupd.enable = true;
 
-  # NTFS Support
-  boot.supportedFilesystems = [ "ntfs" ];
   services.udisks2.enable = true;
 
   boot.kernelPackages = pkgs.linuxPackages_6_4;
-
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.theme = "${../../Pkgs/sddm-chili}";
-
-  programs.waybar.enable = true;
-  programs.fish.enable = true;
-
-  programs.nix-index.enable = true;
-  programs.nix-index.enableFishIntegration = true;
-  programs.nix-index.enableBashIntegration = true;
-  programs.command-not-found.enable = false;
 
   services.gvfs.enable = true; # auto mount thunar
 
@@ -92,23 +33,6 @@
     enable = true;
     # extraPortals = [ inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland ];
   };
-  security.sudo.wheelNeedsPassword = false;
-
-  # BLUETOOTH
-
-  hardware.bluetooth = {
-    enable = true;
-    settings = {
-      General = {
-        Enable = "Source,Sink,Media,Socket";
-      };
-    };
-  };
-  services.blueman.enable = true;
-  hardware.pulseaudio = {
-    package = pkgs.pulseaudioFull;
-  };
-  # hardware.pulseaudio.enable = true;
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
@@ -119,108 +43,10 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Paris";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fr_FR.UTF-8";
-    LC_IDENTIFICATION = "fr_FR.UTF-8";
-    LC_MEASUREMENT = "fr_FR.UTF-8";
-    LC_MONETARY = "fr_FR.UTF-8";
-    LC_NAME = "fr_FR.UTF-8";
-    LC_NUMERIC = "fr_FR.UTF-8";
-    LC_PAPER = "fr_FR.UTF-8";
-    LC_TELEPHONE = "fr_FR.UTF-8";
-    LC_TIME = "fr_FR.UTF-8";
-  };
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "fr";
-    xkbVariant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = "fr";
-
-  users.users.titouan = {
-    isNormalUser = true;
-    description = "titouan";
-    shell = pkgs.fish;
-    extraGroups = [ "networkmanager" "wheel" "audio" "video" "docker" "adbusers" "dialout" ];
-    packages = with pkgs; [ ];
-  };
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  hardware.nvidia = {
-    # modesetting.enable = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-  # services.xserver.videoDrivers = [ "nvidia" "modesetting" "fbdev" ];
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    helix
-    wget
-    git
-    kitty
-    rofi-wayland
-    brave
-    vscodium
-    libsForQt5.qt5.qtgraphicaleffects # sddm
-    xfce.tumbler
-    xfce.thunar
-    xfce.thunar-dropbox-plugin
-    xfce.thunar-media-tags-plugin
-    xfce.thunar-archive-plugin
-    zip
-    wpaperd
-    ripgrep
-    swaynotificationcenter
-
-    pavucontrol
-
-    btop
-
-    lshw
-    glxinfo
-
-    direnv
-    nixpkgs-fmt
-
-    gpu-viewer
-    nvitop
-    unigine-valley
-    unigine-heaven
-
-    imagemagick # utils
-    vulkan-tools # utils-gpu
-    # inputs.eww.packages.${pkgs.system}.eww-wayland
-    (callPackage "${self}/Pkgs/nixer" { })
-    (callPackage "${self}/Pkgs/sddm-chili" { username = "titouan"; }) # for sddm
-  ];
   security.polkit.enable = true;
-
-  # hardware.steam-hardware.enable = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.knownHosts."github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
-  security.pam.enableSSHAgentAuth = true;
-  security.pam.services.sudo.sshAgentAuth = true;
-  services.openssh.settings.PasswordAuthentication = false;
-  programs.ssh.startAgent = true;
-
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
--- Host/laptop/modules.nix
+++ Host/laptop/modules.nix
@@ -6,6 +6,64 @@
     virtualisation.enable = true;
     virtualisation.virtualbox.enable = true;
     virtualisation.docker.enable = true;
-    virtualisation.waydroid.enable = true;
+    virtualisation.waydroid.enable = false;
+    thunar.enable = true;
+    gaming.enable = true;
+    bluetooth.enable = true;
+    jellyfin.enable = false;
+    udev.STM32DISCOVERY.enable = true;
+    udev.ArduinoMega.enable = true;
+    dualboot = {
+      enable = true;
+      windowsUUID = "3EBE-7C56";
+    };
+    general.sessionVariables = {
+      XCURSOR_SIZE = "128";
+      XCURSOR_PATH = [
+        "${config.system.path}/share/icons"
+        "$HOME/.icons"
+        "$HOME/.nix-profile/share/icons/"
+      ];
+      GTK_DATA_PREFIX = [
+        "${config.system.path}"
+      ];
+      NIXOS_DOTFILE_DIR = "${self}";
+    };
+    general.systemPackages = with pkgs; [
+      neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      helix
+      wget
+      git
+      kitty
+      rofi-wayland
+      brave
+      vscodium
+      libsForQt5.qt5.qtgraphicaleffects # sddm
+
+      zip
+      wpaperd
+      ripgrep
+      swaynotificationcenter
+
+      pavucontrol
+
+      btop
+
+      lshw
+      glxinfo
+
+      direnv
+      nixpkgs-fmt
+      home-manager
+
+      gpu-viewer
+      nvitop
+      unigine-valley
+      unigine-heaven
+
+      imagemagick # utils
+      vulkan-tools # utils-gpu
+      (callPackage "${self}/Pkgs/nixer" { })
+    ];
   };
 }
