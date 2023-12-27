d6d6990ac6a1dc0241b8e1ba211f9104916b1b0b
Tol0kk <personal@tolok.org>
2023-12-27 22:23:51 +0100

    Clean

--- Lib/overlay.nix
+++
@@ -1,7 +0,0 @@
-{ inputs, self, ... }: [
-  (self: super: {
-    # my-assets = super.callPackage ../../Pkgs/MyAssets/default.nix { };
-    # eww-wayland = super.callPackage ../Pkgs/eww { withWayland = true; };
-
-  })
-]
