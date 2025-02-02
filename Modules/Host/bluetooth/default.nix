19b1d05f32bc0fe2e7aa2e97737293617e6998ed
Tol0kk <personal@tolok.org>
2025-02-02 19:38:37 +0100

    Add options for Nintendo Switch Pro Controler

--- Modules/Host/bluetooth/default.nix
+++ Modules/Host/bluetooth/default.nix
@@ -21,9 +21,23 @@ in {
       settings = {
         General = {
           Enable = "Source,Sink,Media,Socket";
+          experimental = true; # show battery
+          Privacy = "device";
+          JustWorksRepairing = "always";
+          Class = "0x000100";
+          FastConnectable = true;
         };
       };
     };
     services.blueman.enable = true;
+    # Xbox Controller
+    hardware.xpadneo.enable = true;
+    hardware.xone.enable = true;
+    boot = {
+      extraModulePackages = with config.boot.kernelPackages; [xpadneo];
+      extraModprobeConfig = ''
+        options bluetooth disable_ertm=0
+      '';
+    };
   };
 }
