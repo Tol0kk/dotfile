888e87f8e989b2414f68a29e4b1a9407ad76917d
Tol0kk <personal@tolok.org>
2025-02-03 12:55:45 +0100

    Server: Add Updatime Kuma

---
+++ Modules/Host/server/uptime-kuma/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.modules.server.uptime-kuma;
+  _serverDomain = config.modules.server.cloudflared.domain;
+  _tunnelId = config.modules.server.cloudflared.tunnelId;
+  _domain = "sso.${serverDomain}";
+in {
+  options.modules.server.uptime-kuma = {
+    enable = mkOption {
+      description = "Enable Uptime Kuma service";
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config =
+    mkIf cfg.enable
+    {
+      services.uptime-kuma = {
+        enable = true;
+        # see https://github.com/louislam/uptime-kuma/wiki/Environment-Variables for supported values.
+        settings = {
+          PORT = "8000";
+        };
+      };
+    };
+}
