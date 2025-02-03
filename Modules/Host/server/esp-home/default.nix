4ae236614c2037c7dddb3c88f5cd0b1ab23d046c
Tol0kk <personal@tolok.org>
2025-02-03 23:39:31 +0100

    Servrock: Add ESP-Home Module

---
+++ Modules/Host/server/esp-home/default.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.modules.server.esp-home;
+in {
+  options.modules.server.esp-home = {
+    enable = mkOption {
+      description = "Enable Esp Home service";
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config =
+    mkIf cfg.enable
+    {
+      # TODO see https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=esphome
+      services.esphome.enable = true;
+    };
+}
