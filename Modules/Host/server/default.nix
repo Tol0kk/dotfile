49afdeab44523a8b00a54dc95b84fc8dcb3d2535
Tol0kk <personal@tolok.org>
2025-02-10 17:47:05 +0100

    Common: Make server to use new libs

--- Modules/Host/server/default.nix
+++ Modules/Host/server/default.nix
@@ -1,57 +1,11 @@
 {
   self,
   lib,
+  libDirs,
   ...
 }: let
-  inherit (builtins) readDir pathExists;
-  inherit (lib) assertMsg filterAttrs mapAttrsToList flatten;
-
-  is-directory-kind = kind: kind == "directory";
-
-  ## Safely read from a directory if it exists.
-  ## Example Usage:
-  ## ```nix
-  ## safe-read-directory ./some/path
-  ## ```
-  ## Result:
-  ## ```nix
-  ## { "my-file.txt" = "regular"; }
-  ## ```
-  #@ Path -> Attrs
-  safe-read-directory = path:
-    if pathExists path
-    then readDir path
-    else {};
-
-  ## Get directories at a given path.
-  ## Example Usage:
-  ## ```nix
-  ## get-directories ./something
-  ## ```
-  ## Result:
-  ## ```nix
-  ## [ "./something/a-directory" ]
-  ## ```
-  #@ Path -> [Path]
-  get-directories = path: let
-    entries = safe-read-directory path;
-    filtered-entries = filterAttrs (name: kind: is-directory-kind kind) entries;
-  in
-    mapAttrsToList (name: kind: "${path}/${name}") filtered-entries;
-
+  inherit (libDirs) get-directories;
   server_modules = get-directories ./.;
-  #  server_modules = (
-  #    builtins.map
-  #    (dir: "${self}/Modules/Host/server/" + dir)
-  #    (
-  #      builtins.filter
-  #      (name: !lib.strings.hasSuffix ".nix" name || !lib.strings.hasSuffix ".yaml" name)
-  #      (
-  #        builtins.attrNames
-  #        (builtins.readDir "${self}/Modules/Host/server/")
-  #      )
-  #    )
-  #  );
 in {
   imports = server_modules;
 }
