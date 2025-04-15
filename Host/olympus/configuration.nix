5ec1057b86409a123ab090d8a9793f161349e150
Tol0kk <personal@tolok.org>
2025-04-15 15:34:41 +0200

    Fix olympus configuration

--- Host/olympus/configuration.nix
+++ Host/olympus/configuration.nix
@@ -14,8 +14,8 @@
         tunnelId = "ab1ecc34-4d1c-4356-88e7-ba7889c654ad";
       };
       media-center = {
-        deluge = true;
-        jellyfin = true;
+        deluge.enable = true;
+        jellyfin.enable = true;
       };
       forgejo.enable = true;
       grafana.enable = true;
