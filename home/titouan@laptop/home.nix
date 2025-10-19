dabbe8bb20d72a5807938b32c07d4c2f4900de5b
Tol0kk <titouan.le.dilavrec@gmail.com>
2025-10-19 14:20:21 +0200

    home/synthing update laptop

--- home/titouan@laptop/home.nix
+++ home/titouan@laptop/home.nix
@@ -27,9 +27,9 @@ with libCustom;
 
   services.syncthing.settings = {
     devices = {
-      # "device1" = {
-      #   id = "DEVICE-ID-GOES-HERE";
-      # };
+      "desktop" = {
+        id = "Z2AZUHX-JTHSCXG-FDVIMEB-RAAU7UR-3LQOFHO-5F4ZBJF-56G5HMN-X525BQJ";
+      };
       # "device2" = {
       #   id = "DEVICE-ID-GOES-HERE";
       # };
@@ -37,30 +37,34 @@ with libCustom;
     folders = {
       "Documents" = {
         path = "~/Documents";
+        devices = [ "desktop" ];
       };
       "dev" = {
         path = "~/dev";
       };
       "Pictures" = {
         path = "~/Pictures";
+        devices = [ "desktop" ];
       };
-      "config" = {
-        path = "~/.config";
-      };
-      "Video" = {
-        path = "~/Video";
+      "Videos" = {
+        path = "~/Videos";
+        devices = [ "desktop" ];
       };
       "journal" = {
         path = "~/journal";
+        devices = [ "desktop" ];
       };
       "Music" = {
         path = "~/Music";
+        devices = [ "desktop" ];
       };
       "Templates" = {
         path = "~/Templates";
+        devices = [ "desktop" ];
       };
-      "Game/Sync" = {
-        path = "~/Game/Sync";
+      "Games/Sync" = {
+        path = "~/Games/Sync";
+        devices = [ "desktop" ];
       };
     };
   };
